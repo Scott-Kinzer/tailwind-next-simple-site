0729febfaba64ba8a58e47c1a1833c29d35492d2
Scott-Kinzer <zaz765831@gmail.com>
2023-07-21 11:55:12 +0300

    chore: removed redundant code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,27 +49,6 @@ export default function Home() {
     setTouchEndY(0);
   };
 
-  useEffect(() => {
-    // Handler to call on window resize
-    function handleResize() {
-      sections[activeSection]?.current?.scrollIntoView({
-        behavior: 'smooth',
-      });
-    }
-    // Add event listener
-    window.addEventListener('resize', handleResize);
-    // Call handler right away so state gets updated with initial window size
-    handleResize();
-    // Remove event listener on cleanup
-    return () => window.removeEventListener('resize', handleResize);
-  }, [activeSection]); // Empty array ensures that effect is only run on mount
-
-  // useEffect(() => {
-  //   sections[activeSection]?.current?.scrollIntoView({
-  //     behavior: 'smooth',
-  //   });
-  // }, [activeSection]);
-
   return (
     <main className="">
       <IntroSection
